95559d02ebd5b6f16182f5b0ccb88e6ec858ff6d
luniki <luniki@gmail.com>
2015-02-03 14:09:07 +0100

    enlarge message navbar

--- views/mails/_index_footer.php
+++ views/mails/_index_footer.php
@@ -1,5 +1,5 @@
 <div data-role="footer" data-id="mails-index-footer" data-position="fixed">
-  <div data-role="navbar" data-iconpos=left>
+  <div data-role="navbar">
     <ul>
       <li><a class="<?= $selected === 'inbox'   ? 'ui-btn-active' : '' ?>" data-icon="arrow-r" href="<?= $controller->url_for("mails/inbox")       ?>">Eingang</a></li>
       <li><a class="<?= $selected === 'outbox'  ? 'ui-btn-active' : '' ?>" data-icon="arrow-l" href="<?= $controller->url_for("mails/outbox") ?>">Ausgang</a></li>
